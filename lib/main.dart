e8ce6e97e19c70b2effa8e7297a3d0f75c9628bd
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-09-06 21:12:30 +0500

    dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:zegosocial/views/auth/views/login_page.dart';
 import 'package:zegosocial/views/auth/views/signup_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
